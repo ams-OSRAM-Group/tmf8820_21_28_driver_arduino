fc7404eaff1843aba61149d38d824b80256585c9
MP020422 <mp020422-amsOSRAM@users.noreply.github.com>
2024-04-05 09:48:58 +0200

    - committed TMF8828_Driver_Arduino_Source_v12

---
+++ how_to_generate_image_from_hex/generate_tof_image.bat
@@ -0,0 +1,3 @@
+srec_cat.exe -o tmf8828_image.c -C-Array tmf8828_image -INClude tmf8828_application_patch.hex -Intel
+srec_cat.exe -o tmf882x_image.c -C-Array tmf882x_image -INClude tmf8x2x_application_patch.hex -Intel
+
